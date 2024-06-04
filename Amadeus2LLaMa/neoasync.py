a8b400f8a7e037a67bf76f80400bbfe0d2d16a74
ninefid <denis2006d@gmail.com>
2024-06-05 00:59:20 +0500

    remove unused

--- Amadeus2LLaMa/neoasync.py
+++ Amadeus2LLaMa/neoasync.py
@@ -387,89 +387,10 @@ async def hardware(message, operator: discord.Option(str,name="exec",description
     if len(str(ans))>1994:
         ans=str(ans)[:1991]+"..."
     await message.respond(f"```{ans}```")
-# SERVER
-"""
-@ServComs.command(name_localizations={'en-US':'list','ru':'список'},description_localizations={'en-US':'list available servers','ru':'list av servs'})
-async def servlist(message):
-    ServList={}
-    MaxLen=len(sorted(os.listdir('/mnt/raid1/mcSERVERS'))[-1])
-    for i in os.listdir('/mnt/raid1/mcSERVERS'):
-        for j in os.listdir(f'/mnt/raid1/mcSERVERS/{i}'):
-            if j.endswith('.jar'):
-                ServList[i]=j
-    for i in ServList:
-        OutPut=[str(i+' '*(MaxLen-len(i))+' > '+ServList[i]) for i in ServList]
-    await message.respond('```'+"\n".join(OutPut)+'```')
-    
-    
-async def ConnectToVoiceChannel(message):
-    try:
-        VoiceChannel=message.author.voice.channel
-        await VoiceChannel.connect()
-        Voice=discord.utils.get(client.voice_clients,guild=message.guild)
-        return('Directly')
-    except:
-        try:
-            Voice=message.author.guild.get_member(client.user.id).voice.channel
-            if Voice==VoiceChannel:
-                Voice=discord.utils.get(client.voice_clients,guild=message.guild)
-                Voice.stop()
-                return('AlreadyIn')
-            else:
-                Voice=discord.utils.get(client.voice_clients,guild=message.guild)
-                await Voice.disconnect()
-                await VoiceChannel.connect()
-            return('Moved')
-        except:
-            return('NotConnected')
 
-@client.command(name="name",description="description")
-async def join(message):
-    ans = await ConnectToVoiceChannel(message)
-    if ans == "NotConnected": await message.respond("EC 1")
-    else: await message.respond("EC 0")
-        
-@ServComs.command(name_localizations={'en-US':'status','ru':'статус'},description_localizations={'en-US':'status','ru':'статус'})
-async def status(message):
-    if ServIns=='хуй' or ServIns.poll()!=None:
-        await message.respond('stopped')
-    elif ServIns.poll()==None:
-        await message.respond(f'running > {RunServName}')
 
-@ServComs.command(name_localizations={'en-US':'toggle','ru':'переключить'},description_localizations={'en-US':'Change server state (2-16Gb, 6)','ru':'Изменить состояние сервера (2-16гб, 6)'})
-async def toggle(message,server: discord.Option(str,name_localizations={'en-US': 'name', 'ru': 'название'},description_localizations={'en-US': 'server name', 'ru': 'имя сервера'},required=True,choices=os.listdir(f'/mnt/raid1/mcSERVERS')), mem: discord.Option(int,name_localizations={'en-US': 'mem', 'ru': 'память'},description_localizations={'en-US': 'MB RAM', 'ru': 'Мбайт ОЗУ'},required=False)=6144):
-    if message.author.id in servadminlist:
-        global ServIns, RunServName
-        serverexe=list(filter(lambda x: x.endswith('.jar'),os.listdir(f'/mnt/raid1/mcSERVERS/{server}')))[0]
-        if mem>16384: await message.respond("хули тут так много"); return
-        elif mem<2048: await message.respond("хули тут так мало"); return
-        SerStartInp=["/usr/bin/java", f"-Xmx{mem}M", f"-Xms{mem}M", "-jar" ,f"{serverexe}", "nogui"]
-        if ServIns=='хуй' or ServIns.poll()!=None:
-            await message.respond('now running')
-            RunServName = server
-            ServIns = subprocess.Popen(SerStartInp,stdin=subprocess.PIPE,stdout=subprocess.PIPE,cwd=rf"/mnt/raid1/mcSERVERS/{server}")
-        elif ServIns.poll()==None:
-            await message.respond('already running')
-    else:
-        await message.respond(f'nah')
 
-@ServComs.command(name_localizations={'en-US':'send','ru':'отправить'},description_localizations={'en-US':'Send stdin to terminal','ru':'Отправить stdin в терминал'})
-async def send(message,stdmess: discord.Option(str,name_localizations={'en-US': 'std', 'ru': 'строка'},description_localizations={'en-US': 'stdin input', 'ru': 'stdin ввод'},required=True)):
-    if message.author.id in servadminlist:
-        global ServIns, RunServName
-        if ServIns=='хуй' or ServIns.poll()!=None:
-            await message.respond('stopped')
-        elif ServIns.poll()==None:
-            with MCRcon('localhost','vladgad') as mcr:
-                resp=mcr.command(stdmess)
-                if not resp=="":
-                    await message.respond(resp)
-                else:
-                    await message.respond("EC0")
-    else:
-        await message.respond(f'nah')
-"""
-# =====
+
 
 @client.event
 async def on_voice_state_update(member,before,after):
@@ -494,4 +415,3 @@ if __name__=="__main__":
     print("Starting discord instance...")
     client.run(TOKEN)
 
-LLM_USERS=[573799615074271253,527505544261664798,396961790778540032,579639623668727808,489895341496467456,547738827410898965,676476841707700235,677151784795504651,499442614722887690]
